0721cb2fcb39e629156b35b7ebc3c1c7a55aa8c6
malsorajvazi <malsorajvazi@gmail.com>
2022-03-27 18:41:28 +0200

    Updating Files

--- index.js
+++
@@ -1,9 +0,0 @@
-var name = prompt("What is your name:");
-var firstLetter = name.slice(0,1);
-(firstLetter).toUpperCase;
-var restOfName = name.slice(1,100)
-(restOfName).toLowerCase;
-
-name = firstLetter + restOfName;
-
-alert ("Hello " + name);
